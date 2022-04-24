32e9827184a9a2c4aad116ffbc2953e526d29068
Kristijan <ristovski55@yahoo.com>
2022-04-24 20:35:18 +0200

    routing fix

--- src/main/java/com/example/emtlab/web/BookRestController.java
+++ src/main/java/com/example/emtlab/web/BookRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://library-lab2emt-frontend.herokuapp.com")
 @RequestMapping({"/","/books"})
 public class BookRestController {
 
